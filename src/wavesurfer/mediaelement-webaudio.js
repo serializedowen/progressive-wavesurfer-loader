6bd5c615b3ef0cf4e45774a3e369ca2f50fa13a3
serializedowen <wjh199455@gmail.com>
2020-04-23 16:45:32 +0800

    format

--- src/wavesurfer/mediaelement-webaudio.js
+++ src/wavesurfer/mediaelement-webaudio.js
@@ -1,4 +1,4 @@
-import MediaElement from './mediaelement';
+import MediaElement from "./mediaelement";
 
 /**
  * MediaElementWebAudio backend: allows to load audio as HTML5 audio tag and use it with WebAudio API.
@@ -11,52 +11,50 @@ import MediaElement from './mediaelement';
  * @since 3.2.0
  */
 export default class MediaElementWebAudio extends MediaElement {
-    /**
-     * Construct the backend
-     *
-     * @param {WavesurferParams} params Wavesurfer parameters
-     */
-    constructor(params) {
-        super(params);
-        /** @private */
-        this.params = params;
-        /** @private */
-        this.sourceMediaElement = null;
-    }
+  /**
+   * Construct the backend
+   *
+   * @param {WavesurferParams} params Wavesurfer parameters
+   */
+  constructor(params) {
+    super(params);
+    /** @private */
+    this.params = params;
+    /** @private */
+    this.sourceMediaElement = null;
+  }
 
-    /**
-     * Initialise the backend, called in `wavesurfer.createBackend()`
-     */
-    init() {
-        this.setPlaybackRate(this.params.audioRate);
-        this.createTimer();
-        this.createVolumeNode();
-        this.createScriptNode();
-        this.createAnalyserNode();
-    }
-    /**
-     * Private method called by both `load` (from url)
-     * and `loadElt` (existing media element) methods.
-     *
-     * @param {HTMLMediaElement} media HTML5 Audio or Video element
-     * @param {number[]|Number.<Array[]>} peaks Array of peak data
-     * @private
-     */
-    _load(media, peaks) {
-        super._load(media, peaks);
-        this.createMediaElementSource(media);
-    }
+  /**
+   * Initialise the backend, called in `wavesurfer.createBackend()`
+   */
+  init() {
+    this.setPlaybackRate(this.params.audioRate);
+    this.createTimer();
+    this.createVolumeNode();
+    this.createScriptNode();
+    this.createAnalyserNode();
+  }
+  /**
+   * Private method called by both `load` (from url)
+   * and `loadElt` (existing media element) methods.
+   *
+   * @param {HTMLMediaElement} media HTML5 Audio or Video element
+   * @param {number[]|Number.<Array[]>} peaks Array of peak data
+   * @private
+   */
+  _load(media, peaks) {
+    super._load(media, peaks);
+    this.createMediaElementSource(media);
+  }
 
-    /**
-     * Create MediaElementSource node
-     *
-     * @since 3.2.0
-     * @param {HTMLMediaElement} mediaElement HTML5 Audio to load
-     */
-    createMediaElementSource(mediaElement) {
-        this.sourceMediaElement = this.ac.createMediaElementSource(
-            mediaElement
-        );
-        this.sourceMediaElement.connect(this.analyser);
-    }
+  /**
+   * Create MediaElementSource node
+   *
+   * @since 3.2.0
+   * @param {HTMLMediaElement} mediaElement HTML5 Audio to load
+   */
+  createMediaElementSource(mediaElement) {
+    this.sourceMediaElement = this.ac.createMediaElementSource(mediaElement);
+    this.sourceMediaElement.connect(this.analyser);
+  }
 }
